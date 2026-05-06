e4b47f90197373357b51208ca04c75308e7ccbd9
jaimeperezrisco <jaime.perez00vsl@gmail.com>
2026-05-06 08:33:45 +0100

    initial commit

---
+++ restoreserve/src/main/java/com/example/restoreserve/RestoreserveApplication.java
@@ -0,0 +1,13 @@
+package com.example.restoreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoreserveApplication.class, args);
+	}
+
+}
---
+++ restoreserve/src/test/java/com/example/restoreserve/RestoreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restoreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
